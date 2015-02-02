b4619b70061d44058c55378473deb8173a4eeaf4
JeremyMarshall <jeremystuartmarshall@gmail.com>
2015-02-02 19:09:59 +1100

    fix closures

--- stub.groovy
+++ stub.groovy
@@ -1,30 +1,19 @@
 job (type: Matrix){
-  	  //resolveStrategy = Closure.DELEGATE_ONLY
-  
       name 'auto'
       description 'Build and test the app.'
 
      axes{
-       
-       //m.execute('aaa', {auto()})
-       
-       chuckNorris('aaa'){
-         //name 'name'
+       chuckNorris{
+         name 'name'
          manual 'bbb', 'ccc', 'ddd'
          auto()
        }
-       
-       //chuckNorris('aaa',{name('bbb')})
-       
-       //chuckNorrisManual 'aaa', 'Chuck 1', 'Chuck2'
-       //chuckNorris 'ccc'
-       //text 'ddd', 'a','b','c'
+       text 'ddd', 'a','b','c'
      }
      steps {
-        helloWorld 'Jeremy'
-        //shell('ps')
-        //shell('env |sort')
- 
+        helloWorld {who 'Jeremy'}
+        shell('ps')
+        shell('env |sort')
      }
 }
 
