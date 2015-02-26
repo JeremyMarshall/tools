a85d5cc9883145cba4a6dc56dcab355aea380d4e
JeremyMarshall <jeremystuartmarshall@gmail.com>
2015-02-27 06:14:22 +1100

    rules for log parser

--- swarm.sh
+++ swarm.sh
@@ -1,1 +1,2 @@
-java -jar swarm-client-jar-with-dependencies.jar -master http://jenkins/jenkins -name mac 
+$java -jar swarm-client-jar-with-dependencies.jar -master http://jenkins/jenkins -name mac 
+JOB=$(docker run -d jenkins-slave-mine  /bin/bash -c "java -jar swarm.jar -master=http://192.168.59.3:8080 -username=jeremy -password=jeremy -labels=linx -executors=6")
