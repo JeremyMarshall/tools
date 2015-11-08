ec95ab0f9b358b64ee86fd1c4dabc759344f5e9c
JeremyMarshall <jeremystuartmarshall@gmail.com>
2015-11-08 17:17:49 +1100

    fixup td.pl

--- td.pl
+++ td.pl
@@ -4,6 +4,6 @@ use strict;
 use DBI;
 use Data::Dumper;
 
-my $dbh = DBI->connect("dbi:ODBC:vmtest", 'vmtest', 'vmtest');
+my $dbh = DBI->connect("dbi:ODBC:test", 'dbc', 'dbc');
 
-print Dumper ($dbh->selectall_arrayref(q{select * from vmtest.test}));
+print Dumper ($dbh->selectall_arrayref(q{select count(*) from dbc.tables}));
