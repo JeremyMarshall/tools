890dc0cb7f8b2dad15731cd0d95457bf095dd749
JeremyMarshall <jeremystuartmarshall@gmail.com>
2018-01-21 13:27:30 +1100

    fixed matrix

--- matrix.groovy
+++ matrix.groovy
@@ -1,16 +1,72 @@
 
-void GeneratePermutations(List<List<Character>> Lists, List<String> result, int depth, String current){
-    if(depth == Lists.size()) {
-        result.add(current);        
-        return;      
-    }
+List<Map<String,String>> GeneratePermutations(Map<String,List<String>> axes, List<Map<String,String>> ret){
 
-    for(int i = 0; i < Lists.get(depth).size(); ++i) {         
-        GeneratePermutations(Lists, result, depth + 1, current + Lists.get(depth).get(i));     
-    } 
+	if (axes.size() == 0)
+		return ret
+        else {
+		axes.each { k, v ->
+			if( ret.size() == 0)
+				v.each { i ->
+					ret.push([ "${k}": "${i}"])
+			} else {
+				List<Map<String,String>> newRet = []
+				ret.each { m ->
+					v.each { i ->
+						Map<String,String> n = [:]
+						m.each {k1, v1 ->
+							n[k1] = v1
+						}
+
+						n[k] = i
+						newRet.push(n)
+					}
+				}
+                               ret = newRet 
+			}
+		}
+	}
+        return ret
+	//axes.each{ k, v -> 
+	//	println "${k}:${v}" 
+	//}	
+
 }
 
-List<String> res = []
-GeneratePermutations([['a','b','c'],['x','y','z'],['1','2','3']], res, 0, "")
+println GeneratePermutations([
+	axis1: ['a','b','c'], 
+	axis2: ['x','y','z'], 
+	axis3: ['1','2','3'] ], [])
 
-println (res)
+/*
+[ 
+	[ axis1: 'a', axis2: 'x', axis3: '1'],
+        [ axis1: 'a', axis2: 'x', axis3: '2'],
+	[ axis1: 'a', axis2: 'x', axis3: '3'],
+ 	[ axis1: 'a', axis2: 'y', axis3: '1'],
+	[ axis1: 'a', axis2: 'y', axis3: '2'],
+	[ axis1: 'a', axis2: 'y', axis3: '3'],
+	[ axis1: 'a', axis2: 'z', axis3: '1'],
+	[ axis1: 'a', axis2: 'z', axis3: '2'],
+	[ axis1: 'a', axis2: 'z', axis3: '3'],
+
+        [ axis1: 'b', axis2: 'x', axis3: '1'],
+        [ axis1: 'b', axis2: 'x', axis3: '2'],
+        [ axis1: 'b', axis2: 'x', axis3: '3'],
+        [ axis1: 'b', axis2: 'y', axis3: '1'],
+        [ axis1: 'b', axis2: 'y', axis3: '2'],
+        [ axis1: 'b', axis2: 'y', axis3: '3'],
+        [ axis1: 'b', axis2: 'z', axis3: '1'],
+        [ axis1: 'b', axis2: 'z', axis3: '2'],
+        [ axis1: 'b', axis2: 'z', axis3: '3'],
+
+        [ axis1: 'c', axis2: 'x', axis3: '1'],
+        [ axis1: 'c', axis2: 'x', axis3: '2'],
+        [ axis1: 'c', axis2: 'x', axis3: '3'],
+        [ axis1: 'c', axis2: 'y', axis3: '1'],
+        [ axis1: 'c', axis2: 'y', axis3: '2'],
+        st[ axis1: 'c', axis2: 'y', axis3: '3'],
+        [ axis1: 'c', axis2: 'z', axis3: '1'],
+        [ axis1: 'c', axis2: 'z', axis3: '2'],
+        [ axis1: 'c', axis2: 'z', axis3: '3'],
+]
+*/
